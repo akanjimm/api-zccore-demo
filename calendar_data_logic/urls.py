01596a7230ef4bef7f9ec45ddfb377879e4dad74
Bazzan <bazzanapalowo@gmail.com>
2021-09-23 16:50:24 +0300

    Update Endpoint for serializer

--- calendar_data_logic/urls.py
+++ calendar_data_logic/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
 
-from .views import EventCreateView, event_list_view
+from django.urls import path
+from .views import EventCreateView, event_list_view, EventUpdateView
 
 urlpatterns = [
     path('', event_list_view),
     path('create/', EventCreateView.as_view()),
+    path('update/<str:data_id>/', EventUpdateView.as_view()),
     ]
